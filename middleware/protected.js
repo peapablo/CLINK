cc95dbd3e426d18e681d5efc9849fa949f68e902
Puvadet   Niyomdaychar <98137581+peapablo@users.noreply.github.com>
2025-03-03 00:48:47 +0700

    pinia install in middleware

--- middleware/protected.js
+++ middleware/protected.js
@@ -1,7 +1,10 @@
-export default function ({ store, redirect }) {
-  setTimeout(() => {
-    if (!store.state.accessToken) {
-      return redirect("/login");
-    }
-  }, 1500);
-}
+// middleware/auth.js
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const { $pinia } = useNuxtApp();
+  const store = $pinia.store;  // ใช้ Pinia store แทน Vuex ใน Nuxt 3
+
+  // ตรวจสอบ token
+  if (!store.state.accessToken) {
+    return navigateTo('/login'); // เปลี่ยนเส้นทางไปที่หน้า login ถ้าไม่มี accessToken
+  }
+});
