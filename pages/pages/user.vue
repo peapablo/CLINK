cbd9e59cf3807ed0db8a48ebb54155819356382f
Puvadet   Niyomdaychar <98137581+peapablo@users.noreply.github.com>
2025-03-05 18:57:50 +0700

    restructure pages folder(timeline,user)

--- pages/pages/user.vue
+++ pages/pages/user.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-
     <div class="header pb-6 d-flex align-items-center profile-header">
-      <!-- Mask -->
       <span class="mask bg-gradient-default opacity-8"></span>
-      <!-- Header container -->
       <div class="container-fluid d-flex align-items-center">
         <div class="row">
           <div class="col-lg-7 col-md-10">
             <h1 class="display-2 text-white">Hello Jesse</h1>
             <p class="text-white mt-0 mb-5">This is your profile page. You can see the progress you've made with your
               work and manage your projects or assigned tasks</p>
-            <a href="#!" class="btn btn-neutral">Edit profile</a>
+            <a href="#" class="btn btn-neutral">Edit profile</a>
           </div>
         </div>
       </div>
@@ -20,14 +17,13 @@
     <div class="container-fluid mt--6">
       <div class="row">
         <div class="col-xl-4 order-xl-2">
-          <user-card></user-card>
-          <progress-track-list></progress-track-list>
+          <UserCard />
+          <ProgressTrackList />
         </div>
         <div class="col-xl-8 order-xl-1">
           <div class="row">
             <div class="col-lg-6">
-
-              <card gradient="info" class="border-0">
+              <Card gradient="info" class="border-0">
                 <div class="row">
                   <div class="col">
                     <h5 class="card-title text-uppercase text-muted mb-0 text-white">Total traffic</h5>
@@ -43,16 +39,14 @@
                   <span class="text-white mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
                   <span class="text-nowrap text-light">Since last month</span>
                 </p>
-              </card>
-
+              </Card>
             </div>
             <div class="col-lg-6">
-
-              <card gradient="danger" class="border-0">
+              <Card gradient="danger" class="border-0">
                 <div class="row">
                   <div class="col">
                     <h5 class="card-title text-uppercase text-muted mb-0 text-white">Performance</h5>
-                    <span class="h2 font-weight-bold mb-0 text-white">49,65%</span>
+                    <span class="h2 font-weight-bold mb-0 text-white">49.65%</span>
                   </div>
                   <div class="col-auto">
                     <div class="icon icon-shape bg-white text-dark rounded-circle shadow">
@@ -64,35 +58,27 @@
                   <span class="text-white mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
                   <span class="text-nowrap text-light">Since last month</span>
                 </p>
-              </card>
-
+              </Card>
             </div>
           </div>
-          <edit-profile-form></edit-profile-form>
+          <EditProfileForm />
         </div>
       </div>
     </div>
   </div>
 </template>
-<script>
-  import EditProfileForm from '~/components/pages/UserProfile/EditProfileForm.vue';
-  import UserCard from '~/components/pages/UserProfile/UserCard.vue';
-  import ProgressTrackList from '~/components/widgets/ProgressTrackList.vue';
 
-  export default {
-    layout: 'DashboardLayout',
-    components: {
-      EditProfileForm,
-      UserCard,
-      ProgressTrackList
-    }
-  };
+<script setup>
+import EditProfileForm from "~/components/pages/UserProfile/EditProfileForm.vue";
+import UserCard from "~/components/pages/UserProfile/UserCard.vue";
+import ProgressTrackList from "~/components/widgets/ProgressTrackList.vue";
 </script>
+
 <style>
-  .profile-header {
-    background-image: url(/~/static/img/theme/profile-cover.jpg);
-    background-size: cover;
-    background-position: center top;
-    min-height: 500px;
-  }
+.profile-header {
+  background-image: url("/static/img/theme/profile-cover.jpg");
+  background-size: cover;
+  background-position: center top;
+  min-height: 500px;
+}
 </style>
